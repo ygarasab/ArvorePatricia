2d18c49f823c4e6dfdc03f4bd35ce41d63cc5959
Eduardo Cardoso <eduardo.gil.s.cardoso@gmail.com>
2019-10-08 08:08:36 -0300

    Add files via upload

--- arvore.py
+++ arvore.py
@@ -4,9 +4,22 @@ class Arvore:
 
     def __init__(self, root):
 
+        """
+        Inicia a árvore
+        :param str root:
+        """
+
         self.root = root
 
+
     def get_intersect(self, p1, p2):
+
+        """
+        Buca o ponto onde duas strings de diferenciam
+        :param str p1:
+        :param str p2:
+        :return int:
+        """
 
         for i in range(len(p1)):
 
@@ -14,11 +27,20 @@ class Arvore:
 
                 if p1[i] != p2[i]: return i
 
-            else: break
-
-        return -1
 
     def insere(self, palavra):
+
+        """
+        Insere uma string na árvore
+        :param str palavra:
+        :return None:
+        """
+
+        palavra += '$'
+
+        if self.check(palavra):
+
+            print("[Erro] Palavra já inserida")
 
         pai = None
         node = self.root
@@ -26,34 +48,38 @@ class Arvore:
 
         while 1:
 
+            # Caso nó folha
+
             if type(node) == str:
 
                 pos = self.get_intersect(palavra, node)
 
-                if pos >= 0:
+                # Caso nó folha é raiz também
 
-                    if pai is None:
+                if pai is None:
 
-                        l = sorted([palavra, node])
+                    l = sorted([palavra, node])
 
-                        self.root = Node(pos, l[0][pos], l[0][:pos])
-                        self.root.filhos = l
+                    self.root = Node(pos, l[0][pos], l[0][:pos])
+                    self.root.filhos = l
 
-                        print(self.root)
-                        return
+                    print(self.root)
+                    return
 
-                    else:
 
-                        lado = pai.filhos.index(node)
+                # Caso padrão
 
-                        l = sorted([palavra, node])
+                else:
 
-                        pai.filhos[lado] = Node(pos, l[0][pos], l[0][:pos])
-                        pai.filhos[lado].filhos = l
+                    lado = pai.filhos.index(node)
 
-                        print(pai.filhos[lado])
-                        return
+                    l = sorted([palavra, node])
 
+                    pai.filhos[lado] = Node(pos, l[0][pos], l[0][:pos])
+                    pai.filhos[lado].filhos = l
+
+                    print(pai.filhos[lado])
+                    return
 
 
 
@@ -61,28 +87,35 @@ class Arvore:
 
                 prox = node.get(palavra)
 
+                # Caso prefixo não combina
+
                 if not prox:
 
                     pos = self.get_intersect(palavra, node.prefixo)
 
-                    if pos >= 0:
+                    # Caso raiz
 
-                        if pai is None:
+                    if pai is None:
 
-                            self.root = Node(pos,palavra[pos], palavra[:pos])
+                        self.root = Node(pos,palavra[pos], palavra[:pos])
 
-                            self.root.filhos = [palavra, node] if node.prefixo > palavra else [node, palavra]
+                        self.root.filhos = [palavra, node] if node.prefixo > palavra else [node, palavra]
 
-                            print(self.root)
+                        print(self.root)
 
-                            return
+                        return
+
+                # Base de iteração
 
                 else:
 
                     pai = node
                     node = prox
 
+
     def check(self, palavra):
+
+        palavra += '$'
 
         node = self.root
 
@@ -103,6 +136,66 @@ class Arvore:
                 return False
 
 
+    def remove(self, palavra):
+
+        if not self.check(palavra):
+
+            print("[Erro] Palavra não encontrada")
+            return
+
+        palavra += '$'
+
+        avo = None
+        pai = None
+        node = self.root
+
+        while 1:
+
+            if type(node) == str:
+
+                if pai is None:
+
+                    print("[Erro] Não é possível remover único item da arvore")
+                    return
+
+                else:
+
+                    pai.filhos.remove(node)
+                    novo_sucessor = pai.filhos[0]
+
+                    if avo is None:
+
+                        self.root = novo_sucessor
+                        del pai
+
+                        return
+
+                    else:
+
+                        index = avo.filhos.index(pai)
+                        avo.filhos[index] = novo_sucessor
+                        del pai
+
+                        return
+
+            else:
+
+                prox = node.get(palavra)
+
+                if pai is None:
+
+                    pai = node
+                    node = prox
+
+                else:
+
+                    avo = pai
+                    pai = node
+                    node = prox
+
+
+
+
 
 
 
@@ -115,9 +208,8 @@ p.insere('cornada')
 p.insere('ava')
 p.insere('cornarada')
 p.insere('abelha')
-p.insere('a')
+p.insere('ab')
 
-print(p.check('a'))
+p.remove('abelha')
 
-
-
+print(p.check('ava'))
